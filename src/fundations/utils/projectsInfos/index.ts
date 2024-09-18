6a632c08100dd71120b22881bb34e5ca5c5f7a68
unknown <arthursantos.s@hotmail.com>
2024-09-18 04:46:21 -0300

    feat: add GreenHouse in the project list

--- src/fundations/utils/projectsInfos/index.ts
+++ src/fundations/utils/projectsInfos/index.ts
@@ -1,4 +1,5 @@
-export const projectsInfos = [{
+export const projectsInfos = [
+  {
   name: 'ViraBaixo',
   href: 'https://vbgamehub.online/',
   features: [
@@ -43,6 +44,27 @@ export const projectsInfos = [{
 }, {
   name: 'AurionCrud',
   href: 'https://aurioncrud.netlify.app/',
+  features: [
+    {
+      title: 'FEATURE 1',
+      text: 'Slate helps you see how many more days you need to work to reach your financial goal.'
+    },
+    {
+      title: 'FEATURE 2',
+      text: 'Slate helps you see how many more days you need to work to reach your financial goal.'
+    },
+    {
+      title: 'FEATURE 3',
+      text: 'Slate helps you see how many more days you need to work to reach your financial goal.'
+    },
+    {
+      title: 'FEATURE 4',
+      text: 'Slate helps you see how many more days you need to work to reach your financial goal.'
+    },
+  ]
+}, {
+  name: 'GreenHouse',
+  href: 'https://green-house-lp.vercel.app/',
   features: [
     {
       title: 'FEATURE 1',
