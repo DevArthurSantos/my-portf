dacb3fdd6cf9626b318d791fdab11fd211b7a054
unknown <arthursantos.s@hotmail.com>
2024-09-26 15:28:45 -0300

    feat: add infos in the projects

--- src/fundations/utils/projectsInfos/index.ts
+++ src/fundations/utils/projectsInfos/index.ts
@@ -1,86 +1,88 @@
 export const projectsInfos = [
   {
-  name: 'ViraBaixo',
-  href: 'https://vbgamehub.online/',
-  features: [
-    {
-      title: 'FEATURE 1',
-      text: 'Slate helps you see how many more days you need to work to reach your financial goal.'
-    },
-    {
-      title: 'FEATURE 2',
-      text: 'Slate helps you see how many more days you need to work to reach your financial goal.'
-    },
-    {
-      title: 'FEATURE 3',
-      text: 'Slate helps you see how many more days you need to work to reach your financial goal.'
-    },
-    {
-      title: 'FEATURE 4',
-      text: 'Slate helps you see how many more days you need to work to reach your financial goal.'
-    },
-  ]
-}, {
-  name: 'HPCI',
-  href: 'https://hpic.app/',
-  features: [
-    {
-      title: 'FEATURE 1',
-      text: 'Slate helps you see how many more days you need to work to reach your financial goal.'
-    },
-    {
-      title: 'FEATURE 2',
-      text: 'Slate helps you see how many more days you need to work to reach your financial goal.'
-    },
-    {
-      title: 'FEATURE 3',
-      text: 'Slate helps you see how many more days you need to work to reach your financial goal.'
-    },
-    {
-      title: 'FEATURE 4',
-      text: 'Slate helps you see how many more days you need to work to reach your financial goal.'
-    },
-  ]
-}, {
-  name: 'AurionCrud',
-  href: 'https://aurioncrud.netlify.app/',
-  features: [
-    {
-      title: 'FEATURE 1',
-      text: 'Slate helps you see how many more days you need to work to reach your financial goal.'
-    },
-    {
-      title: 'FEATURE 2',
-      text: 'Slate helps you see how many more days you need to work to reach your financial goal.'
-    },
-    {
-      title: 'FEATURE 3',
-      text: 'Slate helps you see how many more days you need to work to reach your financial goal.'
-    },
-    {
-      title: 'FEATURE 4',
-      text: 'Slate helps you see how many more days you need to work to reach your financial goal.'
-    },
-  ]
-}, {
-  name: 'GreenHouse',
-  href: 'https://green-house-lp.vercel.app/',
-  features: [
-    {
-      title: 'FEATURE 1',
-      text: 'Slate helps you see how many more days you need to work to reach your financial goal.'
-    },
-    {
-      title: 'FEATURE 2',
-      text: 'Slate helps you see how many more days you need to work to reach your financial goal.'
-    },
-    {
-      title: 'FEATURE 3',
-      text: 'Slate helps you see how many more days you need to work to reach your financial goal.'
-    },
-    {
-      title: 'FEATURE 4',
-      text: 'Slate helps you see how many more days you need to work to reach your financial goal.'
-    },
-  ]
-}]
+    name: 'ViraBaixo',
+    href: 'https://vbgamehub.online/',
+    features: [
+      {
+        title: 'Integração',
+        text: 'Utilizei Next.js para construir uma interface interativa e responsiva, melhorando a experiência do usuário.'
+      },
+      {
+        title: 'Backend',
+        text: 'Desenvolvi o backend em Node.js, garantindo performance e escalabilidade para suportar o aumento no número de jogadores e dados.'
+      },
+      {
+        title: 'Real-Time',
+        text: 'Implementei WebSockets para atualizações em tempo real, permitindo uma comunicação instantânea entre os jogadores e o sistema.'
+      },
+      {
+        title: 'Armazenamento',
+        text: 'Utilizei MySQL para gerenciar os dados dos jogadores, garantindo flexibilidade e eficiência no armazenamento e consulta de informações.'
+      }
+    ]
+  },
+  {
+    name: 'HPCI',
+    href: 'https://hpic.app/',
+    features: [
+      {
+        title: 'Escalabilidade',
+        text: 'Projetei a aplicação utilizando Nest.js para criar uma arquitetura modular, facilitando a manutenção e expansão dos serviços hospitalares.'
+      },
+      {
+        title: 'Desenvolvimento',
+        text: 'Implementei TypeScript no desenvolvimento do backend para garantir tipagem segura e prevenir erros, aumentando a robustez da aplicação.'
+      },
+      {
+        title: 'Autenticação',
+        text: 'Implementei autenticação com JSON Web Tokens (JWT), garantindo a segurança dos dados e o controle de acesso dos usuários.'
+      },
+      {
+        title: 'PostgreSQL',
+        text: 'Utilizei PostgreSQL e TypeORM para armazenar e gerenciar grandes volumes de dados médicos de maneira eficiente e segura.'
+      }
+    ]
+  },
+  {
+    name: 'AurionCrud',
+    href: 'https://aurioncrud.netlify.app/',
+    features: [
+      {
+        title: 'Interface',
+        text: 'Desenvolvi a interface utilizando Next.js para garantir uma interação fluida e dinâmica com os usuários ao configurar suas APIs.'
+      },
+      {
+        title: 'API Flexível',
+        text: 'Implementei APIs flexíveis e robustas com Nest.js, permitindo uma rápida criação de endpoints personalizáveis.'
+      },
+      {
+        title: 'Automação',
+        text: 'Configurei Docker para facilitar o ambiente de desenvolvimento e garantir que as APIs possam ser implantadas rapidamente em qualquer servidor.'
+      },
+      {
+        title: 'Banco de Dados',
+        text: 'Utilizei SQLite e Prisma para armazenamento rápido e leve, ideal para a criação de protótipos e desenvolvimento inicial das APIs.'
+      }
+    ]
+  },
+  {
+    name: 'GreenHouse',
+    href: 'https://green-house-lp.vercel.app/',
+    features: [
+      {
+        title: 'Interface',
+        text: 'Implementei a interface com Next.js, garantindo carregamento otimizado e uma navegação rápida para os usuários que monitoram suas estufas.'
+      },
+      {
+        title: 'Controle',
+        text: 'Desenvolvi o controle dos dispositivos de monitoramento e automação da estufa com Node.js, permitindo integrações em tempo real.'
+      },
+      {
+        title: 'Armazenamento',
+        text: 'Implementei MySQL para armazenar dados históricos das condições da estufa, permitindo uma análise completa do crescimento das plantas.'
+      },
+      { title: 'Desenvolvimento', 
+        text: 'TypeScript para segurança e prevenção de erros.' },
+    ]
+  }
+];
