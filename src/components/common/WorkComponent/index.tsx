b7fdefbf2ee93e3097fde167984126037be37144
unknown <arthursantos.s@hotmail.com>
2024-09-26 15:34:54 -0300

    fix: workComponent Moment

--- src/components/common/WorkComponent/index.tsx
+++ src/components/common/WorkComponent/index.tsx
@@ -8,6 +8,8 @@ type WorkComponentProps = {
 
 export default function WorkComponent({ company, date, position, text }: WorkComponentProps) {
 
+  const isMoment = date.split(' ')[date.split(' ').length-1] === 'momento' ? true : false
+
   return (
     <li className="relative max-w-[1000px] flex bg-backgroundSecondary flex-col p-4 rounded-md m-auto items-start justify-start gap-4 text-sm lg:text-lg md:flex-row md:w-full md:bg-transparent md:justify-between">
       <div className="flex items-start flex-col gap-4 max-w-[80px]">
@@ -15,7 +17,7 @@ export default function WorkComponent({ company, date, position, text }: WorkCom
         <h3 className="font-bold text-[12px] text-textSecondary whitespace-nowrap ">{date}</h3>
       </div>
       <div className="absolute md:hidden lg:block md:relative top-2 right-2 border-dashed p-1 border-2 border- border-black rounded-full">
-        <span className={`h-2 w-2 md:h-5 md:w-5 bg-gradientStart rounded-full flex items-center justify-center text-xl font-bold text-background ${date.split(' ').includes('momento') ? 'bg-gradientStart' : 'bg-black'}`}></span>
+        <span className={`h-2 w-2 md:h-5 md:w-5 rounded-full flex items-center justify-center text-xl font-bold text-background ${isMoment ? 'bg-gradientStart' : 'bg-black'}`}></span>
       </div>
       <div className="flex items-start gap-4 flex-col max-w-[400px]">
         <h3 className="font-bold">{position}</h3>
